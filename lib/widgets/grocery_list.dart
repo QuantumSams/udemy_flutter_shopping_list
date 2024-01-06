f25d18087dc15c06889827948d547028e5854d82
QuantumCat <tranhoangphuchuy04@gmail.com>
2024-01-06 16:50:27 +0700

    Add: fallback screen and swipe to remove

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -12,6 +12,7 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   final List<GroceryItem> _groceryItems = [];
+
   void _addNewItem() async {
     final selectedCat = await Navigator.of(context).push(
       MaterialPageRoute(
@@ -21,14 +22,55 @@ class _GroceryListState extends State<GroceryList> {
     if (selectedCat == null) {
       return;
     }
-
     setState(() {
       _groceryItems.add(selectedCat);
     });
   }
 
+  void _removeItem(GroceryItem itemToRemove) {
+    setState(() {
+      _groceryItems.remove(itemToRemove);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget displayScreen = const Center(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "Nothing here",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Text("Click 'Add' button to enter new grocery")
+          ]),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      displayScreen = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => Dismissible(
+          key: ValueKey(_groceryItems[index].id),
+          onDismissed: (direction) {
+            _removeItem(_groceryItems[index]);
+          },
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(
+              _groceryItems[index].quantity.toString(),
+            ),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
@@ -36,20 +78,7 @@ class _GroceryListState extends State<GroceryList> {
           IconButton(onPressed: _addNewItem, icon: const Icon(Icons.add))
         ],
       ),
-      body: ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(_groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: _groceryItems[index].category.color,
-          ),
-          trailing: Text(
-            _groceryItems[index].quantity.toString(),
-          ),
-        ),
-      ),
+      body: displayScreen,
     );
   }
 }
